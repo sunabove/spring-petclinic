60d4179491ad82d2224aca0efb265e107a7e26fa
sunabove <sunabove@nate.com>
2021-09-24 22:54:20 +0900

    **

--- src/main/java/school/SchoolApplication.java
+++ src/main/java/school/SchoolApplication.java
@@ -19,12 +19,6 @@ package school;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * PetClinic Spring Boot Application.
- *
- * @author Dave Syer
- *
- */
 @SpringBootApplication(proxyBeanMethods = false)
 public class SchoolApplication {
 
